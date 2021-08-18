16eb36fcdafeab721ea10018ca4b6265e0d56f29
Janio Muianga <janiodrey@gmail.com>
2021-08-18 16:04:06 +0200

    Event Props

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,9 +1,9 @@
-function Modal() {
+function Modal(props) {
   return (
     <div className='modal'>
       <p>Are you sure?</p>
-      <button className="btn btn--alt">Cancel</button>
-      <button className="btn">Confirm</button>
+      <button className="btn btn--alt" onClick={props.onClick}>Cancel</button>
+      <button className="btn" onClick={props.onClick}>Confirm</button>
     </div>
   );
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -10,6 +10,10 @@ function Todo(props) {
     setModalIsOpen(true);
   }
 
+  function closeModalHandler(){
+    setModalIsOpen(false);
+  }
+
   return (
     <div className="card">
       <h2>{props.text}</h2>
@@ -18,8 +22,8 @@ function Todo(props) {
           Delete
         </button>
       </div>
-      {modalIsOpen && <Modal />}
-      {modalIsOpen && <Backdrop />}
+      {modalIsOpen && <Modal onClick={closeModalHandler}/>}
+      {modalIsOpen && <Backdrop onClick={closeModalHandler}/>}
     </div>
   );
 }
