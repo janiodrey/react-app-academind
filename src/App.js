2a3d44bcb1b5a779073ab15e41a75d13be22c76a
Janio Muianga <janiodrey@gmail.com>
2021-08-18 15:54:01 +0200

    Introducing State

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import Backdrop from "./components/Backdrop";
-import Modal from "./components/Modal";
 import Todo from "./components/Todo";
 
 function App() {
@@ -8,9 +6,7 @@ function App() {
       <h1>My Todos</h1>
       <Todo text="Learn React" />
       <Todo text="Master React" />
-      <Todo text="Explore the full React course" />
-      <Modal />
-      <Backdrop />
+      <Todo text="Explore the full React course" />      
     </div>
   );
 }
