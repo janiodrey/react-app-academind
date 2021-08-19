90309e467b3a51da016952ad7eea77a95f6a2114
Janio Muianga <janiodrey@gmail.com>
2021-08-19 16:38:28 +0200

    Props Children

--- src/App.js
+++ src/App.js
@@ -2,23 +2,24 @@ import { Route, Switch } from "react-router";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupsPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
     <div>
-      <MainNavigation />
-      <Switch>
-        <Route path="/" exact="true">
-          <AllMeetupsPage></AllMeetupsPage>
-        </Route>
-        <Route path="/new-meetup">
-          <NewMeetupsPage></NewMeetupsPage>
-        </Route>
-        <Route path="/favorites">
-          <FavoritesPage></FavoritesPage>
-        </Route>
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route path="/" exact="true">
+            <AllMeetupsPage></AllMeetupsPage>
+          </Route>
+          <Route path="/new-meetup">
+            <NewMeetupsPage></NewMeetupsPage>
+          </Route>
+          <Route path="/favorites">
+            <FavoritesPage></FavoritesPage>
+          </Route>
+        </Switch>
+      </Layout>
     </div>
   );
 }
--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,19 +1,22 @@
-import css from "./MeetupList.module.css";
+import Card from "../ui/Card";
+import css from "./MeetupItem.module.css";
 
 function MeetupItem(props) {
   return (
     <li className={css.item}>
-      <div className={css.image}>
-        <img src={props.imageUrl} alt={props.title} />
-      </div>
-      <div className={css.content}>
-        <h3>{props.title}</h3>
-        <address>{props.address}</address>
-        <p>{props.description}</p>
-      </div>
-      <div className={css.actions}>
-        <button>To Favorites</button>
-      </div>
+      <Card>
+        <div className={css.image}>
+          <img src={props.imageUrl} alt={props.title} />
+        </div>
+        <div className={css.content}>
+          <h3>{props.title}</h3>
+          <address>{props.address}</address>
+          <p>{props.description}</p>
+        </div>
+        <div className={css.actions}>
+          <button>To Favorites</button>
+        </div>
+      </Card>
     </li>
   );
 }
